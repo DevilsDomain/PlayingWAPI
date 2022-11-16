be551e705270d1ec9c6bebed33137a19691dd6df
DevilsDomain <alaraerzincan@gmail.com>
2022-11-16 10:49:54 +0100

    API shows the desired data on the screen

---
+++ src/components/Joke.vue
@@ -0,0 +1,33 @@
+<script setup>
+	import { ref } from 'vue'
+
+
+	const response = ref({})
+
+  // adding delay
+  await new Promise(resolve => {
+    setTimeout(() => { 
+      resolve()
+    }, 1000)
+  })
+    
+  try {
+    const res = await fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,explicit,racist')
+    console.log(res)
+    response.value = (await res.json())
+    console.log(response.value)
+  } catch (error) {
+    response.value = { answer: 'Error! Could not reach the API. ' + error, image: '' }
+  }
+
+
+  
+
+
+</script>
+
+<template>
+  <p>{{ response.setup}}</p>
+  <p>{{ response.delivery}}</p>
+</template>
+
